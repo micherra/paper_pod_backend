250016c1ecd6c57167d223db5209146337768ccc
Michelle Herrera <mher754@wgu.edu>
2024-08-03 10:18:36 -0400

    Implemented categories and added additional types

---
+++ whitepaper/model/arXiv_category.py
@@ -0,0 +1,8 @@
+from typing import TypedDict
+
+
+class ArXivCategory(TypedDict):
+    name: str
+    group: str
+    category: str
+    description: str
---
+++ whitepaper/model/arXiv_metadata.py
@@ -0,0 +1,5 @@
+from whitepaper.model.arXiv_bibtex import ArXivBibtex
+
+
+class ArXivMetadata(ArXivBibtex):
+    abstract: str
