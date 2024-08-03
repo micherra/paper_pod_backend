7d120931723a049f7c2d49bb6285f5b2dfbee0a3
Michelle Herrera <mher754@wgu.edu>
2024-08-03 14:19:39 -0400

    Extracted text mocks to separate files

---
+++ tests/constants/mock_abstract.py
@@ -0,0 +1,11 @@
+ABSTRACT_HTML = """
+        <html>
+            <body>
+                <blockquote class="abstract mathjax">
+                   <span class="descriptor">Abstract:</span>This is a test abstract.
+                </blockquote>
+            </body>
+        </html>
+    """
+
+PARSED_ABSTRACT = "This is a test abstract."
